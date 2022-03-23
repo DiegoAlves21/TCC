94ebe17e8387e5204b51ca510fb1778c31ad5b15
Diego <diego.alves20@outlook.com>
2022-03-23 14:57:56 -0700

    Iniciando projeto

---
+++ backend/src/main/java/br/com/projeto/tcc/TecHealth/TecHealthApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.tcc.TecHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecHealthApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/projeto/tcc/TecHealth/TecHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.tcc.TecHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
